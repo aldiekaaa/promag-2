c49094a947f080e4fdbce7557b568c73100ea670
aldieka332 <aldieka332@github.com>
2023-06-22 11:29:29 +0700

    revisi navbar, login, register

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
 let slideIndex = 0;
-        showSlides();
+showSlides();
 
-        function showSlides() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-            setTimeout(showSlides, 5000); // Change image every 2 seconds
-        }
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
 
-        // Navbar Scroll
-        window.onscroll = function () {
-            const header = document.querySelector('header');
-            const navbarScroll = header.offsetTop;
+// Navbar Scroll
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const navbarScroll = header.offsetTop;
 
-            if (window.pageYOffset > navbarScroll) {
-                header.classList.add('navbar-scroll');
-            } else {
-                header.classList.remove('navbar-scroll');
-            }
-        }
+    if (window.scrollY > navbarScroll) {
+        header.classList.add('navbar-scroll', 'bg-white', 'bg-opacity-20');
+    } else {
+        header.classList.remove('navbar-scroll', 'bg-white', 'bg-opacity-20');
+    }
+}
 
-        // JavaScript Hamburger
-        const hamburger = document.querySelector('#hamburger');
-        const navbar = document.querySelector('#navbar');
+// CAROUSEL LAPORAN TAHUNAN
+var slideIndex = 1;
+showDivs(slideIndex);
 
-        hamburger.addEventListener('click', function () {
-            hamburger.classList.toggle('hamburger-click');
-            navbar.classList.toggle('hidden');
-        });
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("laporanCards");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
