29aed3c3e3eec7611b947fd2a9bc8cee12c62438
aldiekaaa <120025106+aldiekaaa@users.noreply.github.com>
2023-06-24 22:44:16 +0700

    revisi login, register error alert

--- app.js
+++ app.js
@@ -1,42 +1,111 @@
 let slideIndex = 0;
-        showSlides();
+showSlides();
 
-        function showSlides() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-            setTimeout(showSlides, 5000); // Change image every 2 seconds
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000);
+}
+
+// Navbar Scroll
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const navbarScroll = header.offsetTop;
+
+    if (window.pageYOffset > navbarScroll) {
+        header.classList.add('navbar-scroll');
+    } else {
+        header.classList.remove('navbar-scroll');
+    }
+}
+
+// JavaScript Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navbar = document.querySelector('#navbar');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-click');
+    navbar.classList.toggle('hidden');
+});
+
+// PASSWORD VISIBILITY
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password_input");
+    var passwordToggle = document.querySelector(".password-toggle");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordToggle.innerHTML = '<i class="fa-sharp fa-regular fa-eye text-blue-500"></i>';
+    } else {
+        passwordInput.type = "password";
+        passwordToggle.innerHTML = '<i class="fa-sharp fa-regular fa-eye-slash"></i>';
+    }
+}
+
+// function validatePassword(event) {
+//     event.preventDefault();
+
+//     var passwordInput = document.getElementById("password_input");
+//     var passwordAlert = document.querySelector(".password-alert");
+//     var password = passwordInput.value;
+
+//     if (password === 'password123') {
+//         return true;
+//     } else {
+//         passwordInput.classList.add('border-red-500')
+//         passwordAlert.innerHTML = '<p class="text-red-500 text-xs mt-1 ml-3">Password Salah</p>';
+//     }
+// }
+
+// function validateEmail(event) {
+//     event.preventDefault();
+
+//     var emailInput = document.getElementById("email_input");
+//     var emailAlert = document.querySelector(".email-alert");
+//     var email = emailInput.value;
+
+//     if (email === 'email@em.com') {
+//         return true;
+//     } else {
+//         emailInput.classList.add('border-red-500')
+//         emailAlert.innerHTML = '<p class="text-red-500 text-xs mt-1 ml-3">Email Salah</p>';
+//     }
+// }
+
+function validateAccount(event) {
+    event.preventDefault();
+
+    // get the values of all inputs in form account
+    const email = document.getElementById('email_input');
+    const password = document.getElementById('password_input');
+    var passwordAlert = document.querySelector(".password-alert");
+    var emailAlert = document.querySelector(".email-alert");
+    const emailValue = email.value
+    const passValue = password.value
+
+
+    if (emailValue == 'aldi@gmail.com') {
+        if (passValue == 'password123') {
+            alert('Berhasil Register');
+        } else {
+            password.classList.add('border-red-500')
+            passwordAlert.innerHTML = '<p class="text-red-500 text-xs mt-1 ml-3">Password Salah</p>';
         }
-
-        // Navbar Scroll
-        window.onscroll = function () {
-            const header = document.querySelector('header');
-            const navbarScroll = header.offsetTop;
-
-            if (window.pageYOffset > navbarScroll) {
-                header.classList.add('navbar-scroll');
-            } else {
-                header.classList.remove('navbar-scroll');
-            }
-        }
-
-        // JavaScript Hamburger
-        const hamburger = document.querySelector('#hamburger');
-        const navbar = document.querySelector('#navbar');
-
-        hamburger.addEventListener('click', function () {
-            hamburger.classList.toggle('hamburger-click');
-            navbar.classList.toggle('hidden');
-        });
+    } else {
+        email.classList.add('border-red-500')
+        passwordAlert.innerHTML = '<p class="text-red-500 text-xs mt-1 ml-3">Email atau Password Salah</p>';
+    }
+}
